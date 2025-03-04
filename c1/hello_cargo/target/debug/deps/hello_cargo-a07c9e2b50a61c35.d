477b05ce330d298eb7ceda76e73cb5b90aad0cbb
ty <talha.yildirim720@gmail.com>
2025-03-04 13:08:02 -0500

    finished building hello world with cargo - end of chapter1

---
+++ c1/hello_cargo/target/debug/deps/hello_cargo-a07c9e2b50a61c35.d
@@ -0,0 +1,5 @@
+/home/ty/projects/rust/c1/hello_cargo/target/debug/deps/hello_cargo-a07c9e2b50a61c35: src/main.rs
+
+/home/ty/projects/rust/c1/hello_cargo/target/debug/deps/hello_cargo-a07c9e2b50a61c35.d: src/main.rs
+
+src/main.rs:
---
+++ c1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ty/projects/rust/c1/hello_cargo/target/debug/hello_cargo: /home/ty/projects/rust/c1/hello_cargo/src/main.rs
